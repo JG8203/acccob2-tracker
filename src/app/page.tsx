d450c1abab4441591e9a13540db8c0bb3c98aae7
Joshua Armaine Gilo <joshua_gilo@dlsu.edu.ph>
2025-02-24 10:32:36 +0800

    Revert "resized dog"
    This reverts commit 2442542871642079d3fae3df1e0c736bd556ae63.
    
    lmao did not work ung scorlls, worse ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,9 +29,7 @@ export default function AttendanceForm() {
     submitAttendance,
     initialState
   );
-  const [studentOptions, setStudentOptions] = React.useState<
-    { value: string; label: string }[]
-  >([]);
+  const [studentOptions, setStudentOptions] = React.useState<{ value: string; label: string }[]>([]);
   const [student, setStudent] = React.useState<string>("");
 
   React.useEffect(() => {
@@ -42,20 +40,20 @@ export default function AttendanceForm() {
 
     fetchStudentOptions();
 
-    const storedStudent = localStorage.getItem("student");
+    const storedStudent = localStorage.getItem('student');
     if (storedStudent) {
       try {
         const parsedStudent = JSON.parse(storedStudent);
         setStudent(parsedStudent);
       } catch (error) {
-        console.error("Error parsing stored student:", error);
+        console.error('Error parsing stored student:', error);
       }
     }
   }, []);
 
   React.useEffect(() => {
     if (student) {
-      localStorage.setItem("student", JSON.stringify(student));
+      localStorage.setItem('student', JSON.stringify(student));
     }
   }, [student]);
 
@@ -67,21 +65,19 @@ export default function AttendanceForm() {
           <CardDescription>
             Ang hirap mag-encode ng attendance sa Excel 🥲
           </CardDescription>
-          <div className="flex justify-center space-y-2">
           <Image
             src={
               "https://media1.tenor.com/m/ce1bT7v09f4AAAAd/white-dog-shaking.gif"
             }
-            width={300}
-            height={300}
+            width={500}
+            height={500}
             alt="dog gif"
           />
-          </div>
         </CardHeader>
         <CardContent>
-          <form action={action} className="space-y-2" autoComplete="on">
-            <div className="">
-              <div className="">
+          <form action={action} className="space-y-6" autoComplete="on">
+            <div className="space-y-4">
+              <div className="space-y-2">
                 <Label htmlFor="code">Code</Label>
                 <Input
                   id="code"
